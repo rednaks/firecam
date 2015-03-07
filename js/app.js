b4e4e40b693528c9f2585f0d249df703411f3440
Alexandre BM <s@rednaks.tn>
2015-03-07 22:32:25 +0100

    Drawing video stream on canvas

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ window.addEventListener('DOMContentLoaded', function() {
     var video = document.getElementById("stream");
     var canvas = document.getElementById("canvas");
     var button = document.getElementById("start");
+    var streaming = false;
 
     function mediaStreamerHandler(localMediaStream) {
       console.log("Successfully got the user's media");
@@ -18,19 +19,61 @@ window.addEventListener('DOMContentLoaded', function() {
       video.play();
 
     }
-    
+
     function doStartStreaming() {
-    
+
+      if(streaming) {
+        console.log("You're already streaming");
+        return;
+      }
+
         navigator.mozGetUserMedia(
             {video: true, audio:false},
             mediaStreamerHandler,
             function(err){
               console.log(err);
             });
-    
+
     }
 
 
+    function doSendData() {
+
+      console.log("Sending Data");
+
+    }
+    function sendData(){
+      var data = canvas.getDataURL('image/png');
+
+      doSendData(data);
+    }
+
+    function doDrawOnCanvas() {
+
+      try {
+        canvas.getContext('2d').drawImage(video, 0, 0, canvas.width,
+            canvas.height);
+
+        if(!streaming) {
+          console.log("Configuring canvas");
+          canvas.width = video.videoWidth;
+          canvas.height = video.videoHeight;
+
+          if(video.videoWidth != 0)
+            streaming = true;
+        }
+
+      }
+      catch(e) {
+        ;;
+      }
+
+        setTimeout(doDrawOnCanvas, 10);
+
+    }
+
+
+    video.addEventListener('play', doDrawOnCanvas);
     button.addEventListener('click', doStartStreaming);
 
 });
