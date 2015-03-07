cb316aa04a4e8c4db0e39a6a35d345948f700e9c
Alexandre BM <s@rednaks.tn>
2015-03-07 21:26:09 +0100

    Getting user's media

--- js/app.js
+++ js/app.js
@@ -1,5 +1,30 @@
 // app code source
 
+
+
 window.addEventListener('DOMContentLoaded', function() {
-    console.log("Hello World");
+    console.log("Document Loaded !");
+
+    var video = document.getElementById("stream");
+    var canvas = document.getElementById("canvas");
+    var button = document.getElementById("start");
+
+    function mediaStreamerHandler(localMediaStream) {
+      console.log("Successfully got the user's media");
+    }
+    
+    function doStartStreaming() {
+    
+        navigator.mozGetUserMedia(
+            {video: true, audio:false},
+            mediaStreamerHandler,
+            function(err){
+              console.log(err);
+            });
+    
+    }
+
+
+    button.addEventListener('click', doStartStreaming);
+
 });
